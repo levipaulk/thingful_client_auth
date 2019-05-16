c8ead32784623f8149c4dc486b4a7135e0ca0197
Levi Paulk <levipaulk@gmail.com>
2019-05-16 14:25:02 -0400

    end of Thursday

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -5,7 +5,7 @@ const AuthApiService = {
     return fetch(`${config.API_ENDPOINT}/auth/login`, {
       method: 'POST',
       headers: {
-        'content-type': 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(credentials),
     })
@@ -15,6 +15,20 @@ const AuthApiService = {
           : res.json()
       )
   },
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then(res => 
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  }
 }
 
 export default AuthApiService
